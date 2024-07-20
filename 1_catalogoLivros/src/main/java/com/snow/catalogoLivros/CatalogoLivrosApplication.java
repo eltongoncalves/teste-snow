4798f768574fb4ec95f0f2be19d9172d38c8284c
Elton Gonçalves <elton.jd.goncalves@gmail.com>
2024-07-20 16:53:02 -0300

    estrutura api

---
+++ 1_catalogoLivros/src/main/java/com/snow/catalogoLivros/CatalogoLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.snow.catalogoLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoLivrosApplication.class, args);
+	}
+
+}
---
+++ 1_catalogoLivros/src/test/java/com/snow/catalogoLivros/CatalogoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snow.catalogoLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
