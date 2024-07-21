e352e2cdd1a0ea0692e50913c08b343200dd6e0e
Elton Gonçalves <elton.jd.goncalves@gmail.com>
2024-07-21 04:59:16 -0300

    Teste de unitários de controller IdiomaController.java

--- 1_catalogoLivros/src/test/java/com/snow/catalogoLivros/controller/AutorControllerTest.java
+++ 1_catalogoLivros/src/test/java/com/snow/catalogoLivros/controller/AutorControllerTest.java
@@ -17,11 +17,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
